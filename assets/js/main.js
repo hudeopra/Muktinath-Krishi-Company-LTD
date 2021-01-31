1ba5429312caf765d8add4168a2f69112576f133
Prabin Hudeo Maharjan <maharjanhudeo@gmail.com>
2021-01-31 12:33:26 +0545

    desktop complete

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$('.mt-billboard-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    arrows: true,
+    autoplay: true,
+    cssEase: 'linear'
+  });
+  
+  $('.objective-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    cssEase: 'linear'
+  });
